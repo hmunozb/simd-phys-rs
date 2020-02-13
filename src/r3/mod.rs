7ecbf92bfc2f8ebc5a3825b489c9d2e611183e25
hmb <humberto.munozb@gmail.com>
2020-02-12 17:47:28 -0800

    Packet reductions

--- src/r3/mod.rs
+++ src/r3/mod.rs
@@ -54,7 +54,6 @@ pub fn cross_exponential_3d(
         *v_norm = v_sq.sqrt();
     }
     //todo: use parallelizable versions of cos, sin
-    let rc_a_norm = a_norm.map(|v| v.recip());
 
     let mut c_arr = Aligned4xf64::zero();
     let mut s_arr = Aligned4xf64::zero();
@@ -66,6 +65,7 @@ pub fn cross_exponential_3d(
 //    let s_arr = a_norm.map(f64::sin);
     //protect against small-norm division
     let a_norm = a_norm.map(|v| if v.abs() < f64::epsilon(){ 1.0} else { v});
+    let rc_a_norm = a_norm.map(|v| v.recip());
 
     let mut omega_mat : [Aligned4xf64; 9] = [Aligned4xf64::zero(); 9];
     omega_mat[1] = -a[2] ; omega_mat[2] =  a[1];
@@ -101,12 +101,13 @@ pub fn cross_exponential_vector3d(
 ){
     let (ax_sq, ay_sq, az_sq) = (a[0]*a[0], a[1]*a[1], a[2]*a[2]);
     let a_sq = a[0]*a[0] + a[1]*a[1] + a[2]*a[2];
+
     let mut a_norm = Aligned4xf64::default();
     for (v_norm, &v_sq) in a_norm.dat.iter_mut().zip(a_sq.dat.iter()){
         *v_norm = v_sq.sqrt();
     }
     //todo: use parallelizable versions of cos, sin
-    let rc_a_norm = a_norm.map(|v| v.recip());
+
 
     let mut c_arr = Aligned4xf64::zero();
     let mut s_arr = Aligned4xf64::zero();
@@ -116,6 +117,7 @@ pub fn cross_exponential_vector3d(
     }
 
     let a_norm = a_norm.map(|v| if v.abs() < f64::epsilon(){ 1.0} else { v});
+    let rc_a_norm = a_norm.map(|v| v.recip());
 
     let mut omega_mat : Matrix3d4xf64 = Zero::zero();
     omega_mat[(0,1)] = -a[2] ; omega_mat[(0,2)] =  a[1];
@@ -125,8 +127,8 @@ pub fn cross_exponential_vector3d(
 
     let mut omega_sq_mat : Matrix3d4xf64 = Zero::zero();
     omega_sq_mat[(0,0)] = -(ay_sq + az_sq);  omega_sq_mat[(0,1)] = a[0] * a[1];  omega_sq_mat[(0,2)] = a[0] * a[2];
-    omega_sq_mat[(1,0)] = omega_sq_mat[1]; omega_sq_mat[(1,1)] = -(ax_sq + az_sq); omega_sq_mat[(1,2)] = a[1] * a[2];
-    omega_sq_mat[(2,0)] = omega_sq_mat[2]; omega_sq_mat[(2,1)] = omega_sq_mat[5]; omega_sq_mat[(2,2)] = -(ax_sq + ay_sq);
+    omega_sq_mat[(1,0)] = omega_sq_mat[(0, 1)]; omega_sq_mat[(1,1)] = -(ax_sq + az_sq); omega_sq_mat[(1,2)] = a[1] * a[2];
+    omega_sq_mat[(2,0)] = omega_sq_mat[(2, 0)]; omega_sq_mat[(2, 1)] = omega_sq_mat[(1, 2)]; omega_sq_mat[(2,2)] = -(ax_sq + ay_sq);
 
 
     let sinc_a_norm = s_arr * rc_a_norm;
@@ -135,31 +137,39 @@ pub fn cross_exponential_vector3d(
     let cosc_a_norm =(-c_arr + 1.0) * rc_a_norm * rc_a_norm;
     omega_sq_mat *= cosc_a_norm;
 
-    //let mut exp_a = omega_mat;
     *exp_a = Matrix3::identity() + omega_mat + omega_sq_mat;
-//    exp_a[0] += 1.0; exp_a[4] += 1.0; exp_a[8] += 1.0;
-//    for (v, w) in exp_a.iter_mut().zip(omega_sq_mat.iter()){
-//        *v += *w;
-//    }
+
 }
+
 #[cfg(test)]
 mod tests{
     use std;
 
     use crate::vf64::Aligned4xf64;
     use num_traits::{Zero, FloatConst};
-    use crate::r3::cross_exponential_3d;
+    use crate::r3::{cross_exponential_3d, Vector3d4xf64, Matrix3d4xf64, cross_exponential_vector3d};
+    use nalgebra::Vector3;
 
     #[test]
     fn test_cross_exponential_3d(){
         println!("Testing...");
-        let a = [Aligned4xf64::zero(), Aligned4xf64::zero(),
-            Aligned4xf64::from(1.0*f64::PI())];
-        let mut e = [Aligned4xf64::zero(); 9];
+        let theta = 1.5 * f64::PI();
+        //let theta = 0.0;
+        let y = theta / f64::sqrt(2.0);
 
-        cross_exponential_3d(&a, &mut e);
-        for b in e.chunks_exact(3){
-            println!("{}\t{}\t{}", b[0].dat[0], b[1].dat[0], b[2].dat[0]);
+        let a = [Aligned4xf64::zero(), Aligned4xf64::from(y),
+            Aligned4xf64::from(y) ];
+        let a = Vector3d4xf64::from_row_slice(&a);
+        let e = [Aligned4xf64::zero(); 9];
+        let mut e : Matrix3d4xf64 = Matrix3d4xf64::from_column_slice(&e);
+
+        //cross_exponential_3d(&a, &mut e);
+        cross_exponential_vector3d(&a, &mut e);
+
+        for b in e.row_iter(){
+            println!("{:9.8}\t{:9.8}\t{:9.8}", b[0].dat[0], b[1].dat[0], b[2].dat[0]);
         }
+
+
     }
 }
